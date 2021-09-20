cc97a7738318d28abb945764b4ea53f92997b616
IceR <icerbox@gmail.com>
2021-09-20 00:54:57 -0700

     Подготовлена ДЗ по 7 модулю

--- m7/m7/ViewController.swift
+++ m7/m7/ViewController.swift
@@ -2,18 +2,25 @@
 //  ViewController.swift
 //  m7
 //
-//  Created by USER on 09.09.2021.
+//  Created by IceR on 14.09.2021.
+//  Copyright © 2021 Skillbox. All rights reserved.
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    var clicks = 0 // Объявляем счетчик кликов по кнопке и присваиваем 0
+    @IBOutlet weak var Click: UIButton!
+    @IBOutlet weak var LabelText: UILabel!
+    @IBAction func Click(_ sender: UIButton) {
+        clicks += 1 // При каждом клике увеличиваем счетчик на +1
+        LabelText.text = "Нажатий: \(clicks)" // Выводим значение счетчика в лейбл
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Click.backgroundColor = UIColor(red: 0.61, green: 0.98, blue: 0.97, alpha: 1.00) // Через конвертер uicolor.io меняем цвет hex-формата #9bfaf8 на UIColor
     }
-
-
 }
 
