da675e5e4b872884af73bc34264e770781c85f76
IceR <icerbox@gmail.com>
2021-11-17 18:24:27 +0900

    Сделал домашнее задание по модулю 11

---
+++ m11/m11/VideoPreviewCell.swift
@@ -0,0 +1,13 @@
+//
+//  VideoPreviewCell.swift
+//  lesson11
+//
+//  Created by Андрей Антонен on 15.11.2021.
+//
+
+import UIKit
+
+class VideoPreviewCell: UITableViewCell {
+    @IBOutlet weak var videoImage: UIImageView!
+    
+}
